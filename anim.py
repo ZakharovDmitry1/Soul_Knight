4aebadb907003063432c5862fc276f1df6459af7
ZakharovDmitry1 <vbnzvbnz1234567890@gmail.com>
2023-02-01 23:26:20 +0300

    add simple code

--- anim.py
+++ anim.py
@@ -14,6 +14,7 @@ from settings import *
 class Anim(AnimationSprite):
     def __init__(self, sheet: str, list_for_sprites: list[list], x: int, y: int, speed: int = 10, hp: int = 100):
         super(Anim, self).__init__(sheet, list_for_sprites, x, y, TILE_SIZE * 3 // 2)
+        self.rect.size = (TILE_SIZE, TILE_SIZE)
         self.is_moving: bool = True
         self.speed: int = speed
         self.armor = None
