976a8da58b1ac96e175249dca54d76b0e80df012
zaharovdm.24 <vbnzvbnz1234567890@gmail.com>
2023-04-06 15:08:45 +0300

    v

--- player.py
+++ player.py
@@ -25,7 +25,7 @@ class Player(Anim):
 
         self.list_for_sprites = [[0] * 6 for _ in range(4)]
         super(Player, self).__init__(self.sheet, self.list_for_sprites, pos_x, pos_y, speed, hp)
-        self.set_weapon(Gun())
+        self.set_weapon(ShortGun())
         self.hp_bar: Healthbar = Healthbar(self.hp)
         self.mob_radius: int = MOB_RADIUS
 
